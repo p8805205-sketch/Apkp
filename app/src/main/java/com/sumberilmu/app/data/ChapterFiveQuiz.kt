473c5815f1050d72dcb3142d0bb5df19d409567b
p8805205-sketch <p8805205@gmail.com>
2026-06-13 20:18:46 +0700

    Assemble Bab 5 quiz

--- app/src/main/java/com/sumberilmu/app/data/ChapterFiveQuiz.kt
+++ app/src/main/java/com/sumberilmu/app/data/ChapterFiveQuiz.kt
@@ -1,3 +1,10 @@
 package com.sumberilmu.app.data
 
-object ChapterFiveQuiz
+object ChapterFiveQuiz {
+    val questions: List<Question> =
+        ChapterFiveQuizBasics.questions +
+        ChapterFiveQuizAdvanced.questions +
+        ChapterFiveQuizDiagonals.questions +
+        ChapterFiveQuizApplications.questions +
+        ChapterFiveQuizMastery.questions
+}
