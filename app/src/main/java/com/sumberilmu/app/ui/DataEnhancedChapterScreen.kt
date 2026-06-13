2fb60d5f3c1de4d0d9b222ffe3c0a3fbeebb6097
p8805205-sketch <p8805205@gmail.com>
2026-06-13 21:58:49 +0700

    fix: use scoped Column weight in Bab 8 screen

--- app/src/main/java/com/sumberilmu/app/ui/DataEnhancedChapterScreen.kt
+++ app/src/main/java/com/sumberilmu/app/ui/DataEnhancedChapterScreen.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.weight
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
