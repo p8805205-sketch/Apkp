dbbadeeadacdcfe0b59fca004233fd26dc2db2ee
p8805205-sketch <p8805205@gmail.com>
2026-06-13 23:17:10 +0700

    feat: assemble Bab 9 quiz bank

---
+++ app/src/main/java/com/sumberilmu/app/data/ChapterNineQuiz.kt
@@ -0,0 +1,11 @@
+package com.sumberilmu.app.data
+
+object ChapterNineQuiz {
+    val questions: List<Question> =
+        ChapterNineQuizReadingA.questions +
+        ChapterNineQuizReadingB.questions +
+        ChapterNineQuizReadingC.questions +
+        ChapterNineQuizCompareA.questions +
+        ChapterNineQuizMixedA.questions +
+        ChapterNineQuizMixedB.questions
+}
