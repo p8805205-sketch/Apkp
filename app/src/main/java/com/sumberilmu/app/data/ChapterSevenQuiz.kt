1ae9f1f5c0b903c7c2f6c5a353323ebb5587fe49
p8805205-sketch <p8805205@gmail.com>
2026-06-13 21:11:50 +0700

    feat: assemble Bab 7 quiz bank

---
+++ app/src/main/java/com/sumberilmu/app/data/ChapterSevenQuiz.kt
@@ -0,0 +1,8 @@
+package com.sumberilmu.app.data
+
+object ChapterSevenQuiz {
+    val questions: List<Question> =
+        ChapterSevenQuizTriangles.questions +
+        ChapterSevenQuizQuadrilaterals.questions +
+        ChapterSevenQuizComparisons.questions
+}
